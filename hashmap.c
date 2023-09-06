3c5594beb70472dedd69734a92233d147111b91c
Replit user <>
2023-09-06 02:32:55 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -70,7 +70,11 @@ HashMap * createMap(long capacity) {
 
 void eraseMap(HashMap * map,  char * key) {    
   Pair * pair = searchMap(map, key);
-
+  if (pair != NULL) {
+        free(pair->key);
+        pair->key = NULL;
+        map->size++;
+    }
 }
 
 Pair * searchMap(HashMap * map,  char * key) {   
